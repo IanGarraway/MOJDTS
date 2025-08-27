1c7b46256bca8ed161379d8775566d1418af2e12
Ian Garraway <IJGarraway@gmail.com>
2025-08-27 13:11:03 +0100

    chore: refactor header from class to function to be consistent with other components and pages

--- Frontend/DTS-frontend/src/components/Header.jsx
+++ Frontend/DTS-frontend/src/components/Header.jsx
@@ -1,14 +1,8 @@
-import React, { Component } from 'react'
-
-export class Header extends Component {
-  render() {
-    return (
-        <div className='header'>
-            <h2>HMCTS Caseworker</h2>
-            <h1>Task Manager</h1>
-        </div>
-    )
-  }
+export default function Header() {  
+  return (
+    <div className='header'>
+      <h2>HMCTS Caseworker</h2>
+      <h1>Task Manager</h1>
+    </div>
+  );
 }
-
-export default Header
