543545a11f00ab9bcb02485eb9c6aa1e03f70c0e
Ian Garraway <IJGarraway@gmail.com>
2025-08-27 15:53:44 +0100

    chore: refactor of app.test.jsx for clarity

--- Frontend/DTS-frontend/test/Integration/App.test.jsx
+++ Frontend/DTS-frontend/test/Integration/App.test.jsx
@@ -2,8 +2,10 @@ import { render, screen } from '@testing-library/react';
 import App from '../../src/App';
 
 test('App renders Header and Tasks components', () => {
-  render(<App />);
+    //Act
+    render(<App />);
 
-  expect(screen.getByText(/HMCTS Caseworker/i)).toBeInTheDocument(); // Header text
-  expect(screen.getByText(/New Task/i)).toBeInTheDocument(); // Task button from Tasks page
+    //Assert
+    expect(screen.getByText(/HMCTS Caseworker/i)).toBeInTheDocument(); // Header text
+    expect(screen.getByText(/New Task/i)).toBeInTheDocument(); // Task button from Tasks page
 });
