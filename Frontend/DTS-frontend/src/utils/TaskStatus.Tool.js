9315d42bde7a7c66a7d0759572cae49443d6e3de
Ian Garraway <IJGarraway@gmail.com>
2025-08-29 18:09:25 +0100

    chore: add comments to TaskStatus.tool

--- Frontend/DTS-frontend/src/utils/TaskStatus.Tool.js
+++ Frontend/DTS-frontend/src/utils/TaskStatus.Tool.js
@@ -1,5 +1,16 @@
+/**
+ * TaskStatusTool
+ * Provides helper functions for working with task statuses,
+ * including conversion to human-readable labels and colour coding.
+ */
 export default class TaskStatusTool{
 
+    /**
+     * statusToText
+     * Converts a numeric status code into a human-readable label
+     * @param {number} statusCode - The numeric status of a task
+     * @returns {string} - the textural representation of the the status
+     */
     static statusToText(statusCode) {
         return {
             1: "Pending",
@@ -8,6 +19,13 @@ export default class TaskStatusTool{
         }[statusCode]||"Unknown";
     }
 
+    /**
+     * statusToBadgeStyle
+     * Maps a numeric status code to a Bootstrap badge style.
+     *
+     * @param {number} statusCode - The numeric status of a task
+     * @returns {string} - Bootstrap badge class (warning, primary, success, secondary)
+     */
     static statusToBadgeStyle(statusCode) {
         return {
             1: "warning",
@@ -16,5 +34,4 @@ export default class TaskStatusTool{
             
         }[statusCode]||"secondary";
     }
-
 }
